6778cc8d565a3a57d8a786d4b428e810f22b6837
kligtech <kligvasser@gmail.com>
2020-04-17 12:18:48 +0300

    tta in eval

--- trainer.py
+++ trainer.py
@@ -116,10 +116,18 @@ class Trainer():
         # Set inputs
         inputs = data['input'].to(self.device)
         targets = data['target'].to(self.device)
+        inputs_size = inputs.size()
 
         # Run model
         with torch.no_grad():
+            if self.args.tta:
+                inputs = inputs.view(-1, inputs_size[2], inputs_size[3], inputs_size[4])
+
             outputs = self.model(inputs)
+
+            if self.args.tta:
+                outputs = outputs.view(inputs_size[0], inputs_size[1], -1).mean(dim=1)
+
             preds = torch.softmax(outputs, dim=1)
             loss = self.criterion(outputs, targets)
 
@@ -252,7 +260,8 @@ class Trainer():
             self._train_epoch(loaders['train'])
 
             # Eval
-            auc, acc = self._eval_epoch(loaders['eval'], epoch)
+            # auc, acc = self._eval_epoch(loaders['eval'], epoch)
+            auc, acc = self._eval_epoch(loaders['test'], epoch)
             score = auc * self.args.weight_auc + (acc / 100.) * self.args.weight_acc
 
             # Scheduler
