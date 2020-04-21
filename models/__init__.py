e9964cdde523996dd7932e7ec8f2794a478dd1a0
klig <kligvasser@gmail.com>
2020-04-21 14:52:49 +0300

    added resnext and resnet_sd

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,5 @@
 from .resnet import *
+from .resnet_sd import *
+from .resnext import *
 from .efficientnet import *
 from .efficientnet_ns import *
--- models/resnext.py
+++ models/resnext.py
@@ -2,22 +2,16 @@ import torchvision
 import torch.nn as nn
 import torch.nn.functional as F
 
-__all__ = ['resnet']
+__all__ = ['resnext']
 
 class ResNet(nn.Module):
-    def __init__(self, num_classes=4, depth=18, two_stage=False):
+    def __init__(self, num_classes=4, depth=50, two_stage=False):
         super().__init__()
 
-        if depth == 18:
-            self.backbone = torchvision.models.resnet18(pretrained=True)
-        elif depth == 34:
-            self.backbone = torchvision.models.resnet34(pretrained=True)
-        elif depth == 50:
-            self.backbone = torchvision.models.resnet50(pretrained=True)
-        elif depth == 101:
-            self.backbone = torchvision.models.resnet101(pretrained=True)
+        if depth == 50:
+            self.backbone = torchvision.models.resnext50_32x4d(pretrained=True)
         else:
-            self.backbone = torchvision.models.resnet152(pretrained=True)
+            self.backbone = torchvision.models.resnext101_32x8d(pretrained=True)
 
         if two_stage:
             self.classifer = nn.Sequential(nn.Linear(self.backbone.fc.in_features, 1024),
@@ -44,8 +38,8 @@ class ResNet(nn.Module):
 
         return x
 
-def resnet(**config):
-    config.setdefault('depth', 18)
+def resnext(**config):
+    config.setdefault('depth', 50)
     config.setdefault('num_classes', 4)
     config.setdefault('two_stage', False)
 
